4ac5d15d18bc9e684cbd2a493df95c90d9e713b2
IS Oguro <ingrid.shizue@gmail.com>
2026-01-25 00:58:04 -0300

    feat: Utilizando service

--- src/app/pages/painel-acoes/painel-acoes.component.ts
+++ src/app/pages/painel-acoes/painel-acoes.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PainelAcoesService } from '../../services/painel-acoes.service';
+import {JsonPipe} from '@angular/common'
+import { NgIf, NgFor } from '@angular/common';
+import { Acao } from '../../models/acoes.model';
 
 @Component({
   selector: 'app-painel-acoes',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe,NgIf,NgFor],
   templateUrl: './painel-acoes.component.html',
   styleUrl: './painel-acoes.component.css'
 })
-export class PainelAcoesComponent {
+export class PainelAcoesComponent implements OnInit{
+
+  stocks: Acao[] = [];
+  loading = false;
+  error = false;
+
+ constructor(private painelAcoesService: PainelAcoesService) {}
+
+  ngOnInit(): void {
+    this.carregarAcoes();
+  }
+
+    carregarAcoes(): void {
+    this.loading = true;
+    this.error = false;
+
+    this.painelAcoesService.obterAcoes(['ITUB4']).subscribe({
+      next: (response) => {
+        this.stocks = this.mapearAcoes(response.stocks);
+        this.loading = false;
+      },
+      error: () => {
+        this.error = true;
+        this.loading = false;
+      }
+    });
+  }
+
+  mapearAcoes(results: any[]): Acao[] {
+  return results.map((item): Acao => ({
+    stock: item.stock,
+    name: item.name,
+    close: item.close,
+    change:item.change,
+    volume:item.volume,
+    marketcap:item.market_cap,
+    logo:item.logo,
+    sector:item.sector,
+    type:item.type
+  }))
+}
+
 
 }
