76adce4b34a2933ad95528f8540f5dc327a7d680
IS Oguro <ingrid.shizue@gmail.com>
2026-01-25 00:50:46 -0300

    feat: requisição para obter lista de ações

--- src/app/services/painel-acoes.service.ts
+++ src/app/services/painel-acoes.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PainelAcoesService {
+  private baseUrl = 'https://brapi.dev/api/quote/list';
+  private readonly token = 'kMQQvei35hYMNqzH58pm4m';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obterAcoes(tickers: string[]) {
+    const url =
+    `${this.baseUrl}/?limit=50&token=${this.token}`;
+
+  console.log('URL da requisição:', url);
+
+  return this.http.get<any>(url, {
+    responseType: 'json'
+  });
+  }
+
 }
