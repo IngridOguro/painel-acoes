eabc855bfe4412c4bda777cb0597018d37398ed8
IS Oguro <ingrid.shizue@gmail.com>
2026-01-25 00:52:45 -0300

    feat: criar interface de Ação

---
+++ src/app/models/acoes.model.ts
@@ -0,0 +1,11 @@
+export interface Acao {
+  stock: string;
+  name: string;
+  close: number;
+  change:number;
+  volume:number;
+  marketcap:number;
+  logo:string;
+  sector:string;
+  type:string;
+}
